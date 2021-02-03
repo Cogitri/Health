80e4c977a085e2d968e4ec3a926c88fc0c8b47e5
Rasmus Thomsen <oss@cogitri.dev>
2021-02-03 15:21:58 +0100

    test(database): add tests

--- src/core/utils.rs
+++ src/core/utils.rs
@@ -1,4 +1,6 @@
 use gtk::EditableExt;
+#[cfg(test)]
+use std::future::Future;
 
 pub fn get_spinbutton_value<T>(spin_button: &gtk::SpinButton) -> T
 where
@@ -11,4 +13,25 @@ where
         .as_str()
         .parse::<T>()
         .unwrap_or_default()
+}
+
+#[cfg(test)]
+pub fn run_async_test_fn<T: 'static, F: 'static>(future: F) -> T
+where
+    F: Future<Output = T>,
+{
+    let context = glib::MainContext::new();
+    let ml = glib::MainLoop::new(Some(&context), false);
+    let (sender, receiver) = std::sync::mpsc::channel();
+
+    context.push_thread_default();
+    let m = ml.clone();
+    context.spawn_local(async move {
+        sender.send(future.await).unwrap();
+        m.quit();
+    });
+
+    ml.run();
+
+    receiver.recv().unwrap()
 }
