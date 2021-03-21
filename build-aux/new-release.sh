e2b6bd008bb985ad3c43392ac5973f18852de428
Rasmus Thomsen <oss@cogitri.dev>
2021-03-21 15:05:56 +0100

    chore(meson): add release target

---
+++ build-aux/new-release.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+current=$(grep -Po "version: '\K([0-9]*\.[0-9]*.[0-9]+)(?=')" meson.build)
+major=$(cut -d '.' -f1 <<< "$current")
+minor=$(cut -d '.' -f2 <<< "$current")
+patch=$(cut -d '.' -f3 <<< "$current")
+
+case $1 in
+    major)
+        next=$(echo $((major + 1)).0.0)
+        ;;
+    minor)
+        next=$(echo $major.$((minor + 1)).0)
+        ;;
+    patch)
+        next=$(echo $major.$minor.$((patch + 1)))
+        ;;
+    *)
+        echo "Don't know what to do, exiting!"
+        exit 1
+    ;;
+esac
+
+sed -i "s/version: '$current'/version: '$next'/" meson.build
+sed -i "s/version = \"$current\"/version = \"$next\"/" Cargo.toml
+${EDITOR:=nano} data/dev.Cogitri.Health.appdata.xml.in
+
+ninja -C _build test
+
+git commit -av
+git tag $next
+
+ninja -C _build release
---
+++ build-aux/release.sh
@@ -0,0 +1,45 @@
+#!/bin/bash -e
+
+if ! [ "$MESON_BUILD_ROOT" ]; then
+    echo "This can only be run via meson, exiting!"
+    exit 1
+fi
+
+PKGVER=$1-$2
+DEST=${MESON_BUILD_ROOT}
+DIST=$DEST/dist/$PKGVER
+SRC=${MESON_SOURCE_ROOT}
+
+
+cd "${MESON_SOURCE_ROOT}"
+mkdir -p "${DIST}"
+
+ginst() {
+	local pattern f
+	for pattern; do
+		for f in ${pattern#/}; do # let shell expand the pattern
+			# only create dir if needed
+			if [ "${f%/*}" != "$f" ]; then
+				mkdir -p "${DIST}/${f%/*}"
+			fi
+			cp -rv "$SRC"/$f "${DIST}/${f%/*}"
+		done
+	done
+}
+
+for x in $(git ls-files); do
+	ginst "$x"
+done
+
+mkdir -p "${DIST}"/.cargo
+cargo vendor vendor | sed 's/^directory = ".*"/directory = "vendor"/g' > "${DIST}"/.cargo/config
+cp -r vendor "${DIST}"
+
+# packaging
+cd "${DEST}"/dist
+tar cJvf $PKGVER.tar.xz $PKGVER
+
+if type gpg; then
+	gpg --armor --detach-sign $PKGVER.tar.xz
+	gpg --verify $PKGVER.tar.xz.asc $PKGVER.tar.xz
+fi
