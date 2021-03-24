abd5f5daf4e2db04850d1e369aef7afb7a4399cb
Rasmus Thomsen <oss@cogitri.dev>
2021-03-24 15:48:14 +0100

    chore(meson): use meson dist instead of custom command

---
+++ build-aux/dist.sh
@@ -0,0 +1,13 @@
+#!/bin/bash -e
+
+if ! [ "$MESON_BUILD_ROOT" ]; then
+    echo "This can only be run via meson, exiting!"
+    exit 1
+fi
+
+PKGVER=$1-$2
+DIST="$MESON_BUILD_ROOT/meson-dist/$PKGVER"
+
+mkdir -p "${DIST}"/.cargo
+cargo vendor vendor | sed 's/^directory = ".*"/directory = "vendor"/g' > "${DIST}"/.cargo/config
+cp -r vendor "${DIST}"
