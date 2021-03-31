9b934b91f06b2d987f7594efa43f99cb5e3bf0f2
Rasmus Thomsen <oss@cogitri.dev>
2021-03-31 09:52:50 +0200

    refactor(settings): move unitsystem tests to unitsystem.rs

--- src/core/settings.rs
+++ src/core/settings.rs
@@ -139,25 +139,3 @@ impl Settings {
             .unwrap();
     }
 }
-
-#[cfg(test)]
-mod test {
-    use super::Unitsystem;
-    use std::str::FromStr;
-
-    #[test]
-    fn deserialize_unitsystem() {
-        assert_eq!(Unitsystem::from_str("imperial"), Ok(Unitsystem::Imperial));
-        assert_eq!(Unitsystem::from_str("metric"), Ok(Unitsystem::Metric));
-
-        assert!(Unitsystem::from_str("unknown").is_err());
-    }
-
-    #[test]
-    fn serialize_unitsystem() {
-        let a: &str = Unitsystem::Imperial.into();
-        assert_eq!(a, "imperial");
-        let b: &str = Unitsystem::Metric.into();
-        assert_eq!(b, "metric");
-    }
-}
