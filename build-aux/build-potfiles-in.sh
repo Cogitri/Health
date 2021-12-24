6ecd69c0ce043f018bffd9500a7bee1706103c12
Rasmus Thomsen <oss@cogitri.dev>
2021-12-24 13:16:21 +0100

    chore(ci): enforce C sorting

--- build-aux/build-potfiles-in.sh
+++ build-aux/build-potfiles-in.sh
@@ -2,6 +2,9 @@
 
 cd "${MESON_SOURCE_ROOT}"
 
+# ensure proper sorting
+export LC_ALL=C
+
 printf "# Misc data\n%s\n\n# UI Files\n%s\n\n# Source files\n%s\n" \
     "$(find data -name "*.in" | sort -u )" \
     "$(find data -name "*.ui" | sort -u)" \
