c06eea8623c4c9d985ff9bdebc7e4142c5160c1d
Rasmus Thomsen <oss@cogitri.dev>
2021-12-24 13:04:38 +0100

    chore(build-aux): add script to regenerate POTFILES.in

---
+++ build-aux/build-potfiles-in.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env sh
+
+cd "${MESON_SOURCE_ROOT}"
+
+printf "# Misc data\n%s\n\n# UI Files\n%s\n\n# Source files\n%s\n" \
+    "$(find data -name "*.in" | sort -u )" \
+    "$(find data -name "*.ui" | sort -u)" \
+    "$(find src -name "*.rs" | sort -u | tail -n +2)" \
+    > po/POTFILES.in
