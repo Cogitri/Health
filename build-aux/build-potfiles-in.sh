f44d90e0c3e3112d3a7c800ee43d6c696b9216d3
Rasmus Thomsen <oss@cogitri.dev>
2021-12-24 16:10:02 +0100

    chore(po): add gschema to POTFILES.in

--- build-aux/build-potfiles-in.sh
+++ build-aux/build-potfiles-in.sh
@@ -6,7 +6,7 @@ cd "${MESON_SOURCE_ROOT}"
 export LC_ALL=C
 
 printf "# Misc data\n%s\n\n# UI Files\n%s\n\n# Source files\n%s\n" \
-    "$(find data -name "*.in" | sort -u )" \
+    "data/dev.Cogitri.Health.gschema.xml $(find data -name "*.in" | sort -u )" \
     "$(find data -name "*.ui" | sort -u)" \
     "$(find src -name "*.rs" | sort -u | tail -n +2)" \
     > po/POTFILES.in
