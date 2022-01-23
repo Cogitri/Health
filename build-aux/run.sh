d7aa31476eb5265f93680ba5189327528ea521e9
Rasmus Thomsen <oss@cogitri.dev>
2022-01-23 23:00:19 +0100

    chore(run): fix launching in development profile

--- build-aux/run.sh
+++ build-aux/run.sh
@@ -5,4 +5,4 @@ set -e
 export RUST_BACKTRACE=1
 
 ninja -C "${MESON_BUILD_ROOT}"
-meson devenv -C "${MESON_BUILD_ROOT}" "${MESON_BUILD_ROOT}"/src/dev.Cogitri.Health
+meson devenv -C "${MESON_BUILD_ROOT}" "${MESON_BUILD_ROOT}"/src/"${1}"
