6c31e873afabb58a124be16f6b75f77981184624
maxice8 <thinkabit.ukim@gmail.com>
2022-03-15 12:40:48 -0300

    feat(flake): add flake that provides a devShell
    the devShell needs to be called with --impure since we use the mozilla
    overlay for rust and that pulls from a pinned repository in an
    unreproducible way, a big no-no for flakes.

---
+++ flake.nix
@@ -0,0 +1,90 @@
+{
+  description = ''A health tracking app for the GNOME desktop
+  
+  Includes an overlay for usage inside nixpkgs and a development
+  environment'';
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    mozilla.url = "github:mozilla/nixpkgs-mozilla";
+
+    flake-utils.url = "github:numtide/flake-utils";
+    flake-compat = {
+      url = "github:edolstra/flake-compat";
+      flake = false;
+    };
+  };
+
+  outputs = { nixpkgs, mozilla, flake-utils, ... }:
+    let
+      inherit (flake-utils.lib) eachSystem system;
+    in
+    eachSystem [ system.x86_64-linux ]
+      (system:
+        let
+          pkgs = import nixpkgs
+            {
+              inherit system;
+              overlays = [
+                mozilla.overlay
+                (final: prev: {
+                  trackerPatched = pkgs.tracker.overrideAttrs (old: rec {
+                    patches = old.patches ++ [ ./build-aux/tracker-subsecond-accuracy.patch ];
+                  });
+                })
+              ];
+            };
+          rustSrc = pkgs.latest.rustChannels.stable.rust.override { extensions = [ "rust-src" ]; };
+          buildInputs = with pkgs; [
+            appstream-glib
+            cairo
+            cargo-audit
+            cargo-bloat
+            cargo-dephell
+            cargo-expand
+            cargo-outdated
+            clang_13
+            desktop-file-utils
+            gdb
+            gdk-pixbuf
+            glib
+            glib
+            graphene
+            gtk4.dev
+            gtk4
+            harfbuzz
+            libadwaita
+            libfaketime
+            librsvg
+            libsecret
+            libsecret.dev
+            libxml2
+            lld_13
+            meson
+            ninja
+            pango
+            pkg-config
+            python3
+            rustfmt
+            rustSrc
+            trackerPatched
+            trackerPatched.dev
+            valgrind
+            wayland
+            wayland.dev
+          ];
+        in
+        {
+          devShell = pkgs.mkShell
+            {
+              name = "Health-shell";
+              inherit buildInputs;
+              RUST_SRC_PATH = "${rustSrc}/lib/rustlib/src/rust/src";
+              RUSTFLAGS = "-C linker=clang -C link-arg=--ld-path=${pkgs.lld_13}/bin/ld.lld";
+              LD_LIBRARY_PATH = "${pkgs.lib.makeLibraryPath buildInputs}";
+              #LD_PRELOAD = "${pkgs.libfaketime}/lib/libfaketime.so.1";
+              XDG_DATA_DIRS = with pkgs; "${gtk4.dev}/share:{libadwaita.dev}/share:${gdk-pixbuf.dev}/share:${gobject-introspection.dev}/share:${pango.dev}/share:${harfbuzz.dev}/share:${graphene}/share:${libadwaita.dev}/share";
+              RUST_BACKTRACE = 1;
+            };
+        });
+}
