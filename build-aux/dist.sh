d664384370bb55f0e46a78da094c567a5a7e97c6
Rasmus Thomsen <oss@cogitri.dev>
2022-07-21 01:36:47 +0200

    release: release 0.94.0

--- build-aux/dist.sh
+++ build-aux/dist.sh
@@ -9,5 +9,12 @@ PKGVER=$1-$2
 DIST="$MESON_BUILD_ROOT/meson-dist/$PKGVER"
 
 mkdir -p "${DIST}"/.cargo
-cargo vendor vendor | sed 's/^directory = ".*"/directory = "vendor"/g' > "${DIST}"/.cargo/config
+cargo vendor vendor
+cat << EOF > "${DIST}"/.cargo/config
+[source.crates-io]
+replace-with = "vendored-sources"
+
+[source.vendored-sources]
+directory = "vendor"
+EOF
 mv vendor "${DIST}"
