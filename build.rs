fa684c08c545c807ba747302f57785f777df4067
Rasmus Thomsen <oss@cogitri.dev>
2023-07-29 13:51:06 +0200

    chore(build): don't rebuild every time

--- build.rs
+++ build.rs
@@ -38,13 +38,15 @@ pub fn compile_resources(source_dirs: &[&str], gresource: &str, target: &str) {
         .stdout;
     let output = String::from_utf8(output).unwrap();
     for dep in output.split_whitespace() {
+        if dep.contains(".ui") {
+            continue;
+        }
         println!("cargo:rerun-if-changed={}", dep);
     }
 }
 
 pub fn compile_blueprint(source_dir: &str, target: &str) {
     let out_dir = env::var("OUT_DIR").unwrap();
-
     let mut command = Command::new(BLUEPRINT_COMPILER_PATH);
     command
         .arg("batch-compile")
