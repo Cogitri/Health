f82ce12f4059f0f3b345c02b3b824346ffaa76bb
Rasmus Thomsen <oss@cogitri.dev>
2023-07-30 10:08:26 +0200

    chore(ci): run tests in valgrind

--- .gitlab/ci/run-in-flatpak.sh
+++ .gitlab/ci/run-in-flatpak.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
 xvfb-run -a -s "-screen 0 1024x768x24" flatpak-builder --user --disable-rofiles-fuse --build-shell=health flatpak_app dev.Cogitri.Health.Devel.json <<END
+export CARGO_TARGET_X86_64_UNKNOWN_LINUX_GNU_RUNNER="valgrind --error-exitcode=1"
 $@
 END
